ee0e5fd71e441e2119f419cd2e4a38748f3a14e8
Murtadha-Alalawi <murtadha.alawi04@gmail.com>
2024-12-09 19:38:28 +0300

    Creating divs based on the letter count in the word

--- app.js
+++ app.js
@@ -4,33 +4,43 @@ let answerText = document.querySelector("#answer")
 
 let answer = null
 
+
 startButton.addEventListener("click", (event) => {
       answer = "CAT"
+      
+     
+      for(let i = 0; i < answer.length; i++){
+        let div = document.createElement('div')
+        div.className = "answer-letter"
+        div.innerHTML = "_"
+        answerText.appendChild(div)
+        
+      }
+     
+      const answerLetters = document.querySelectorAll(".answer-letter")
+      
+
+
+      letterButtons.forEach((letterButton)=>{
+        letterButton.addEventListener("click", (event)=>{
+            if (answer === null){
+                return
+            }
+            checkLetter(event.target.innerText)
+        })
+    }) 
+    
+    
+    function checkLetter(letter){
+        for(let i = 0; i < answer.length; i++){
+            if(answer[i] === letter){
+                console.log(answerLetters[i])
+                answerLetters[i].textContent = answer[i]
+            }
+        }
+    
+    }
 })
 
 const letterButtons = document.querySelectorAll(".letters")
-
-const answerLetters = document.querySelectorAll(".answer-letter")
-
-
-
-letterButtons.forEach((letterButton)=>{
-    letterButton.addEventListener("click", (event)=>{
-        if (answer === null){
-            return
-        }
-        checkLetter(event.target.innerText)
-    })
-}) 
-
-
-function checkLetter(letter){
-    for(let i = 0; i < answer.length; i++){
-        if(answer[i] === letter){
-            answerLetters[i].textContent = answer[i]
-        }
-    }
-
-}
-
 
